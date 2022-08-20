17747147073692304d530e794c8c33c61db1b08f
Mark Chen <mark670101@gmail.com>
2022-08-20 09:28:21 +0800

    20220820

---
+++ Controllers/CommandController.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CmdApi.Models;
+
+
+
+namespace CmdApi.Controllers
+{
+    [Route("api/[controller]")] /* http://localhost:5000/api/commands */
+    [ApiController]
+    public class CommandsController:ControllerBase
+    {
+        private readonly CommandContext _context;
+
+        public CommandsController(CommandContext context) => _context = context;
+
+        //GET:     /api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetCommands()
+        {
+            return _context.CommandItems;
+        }
+       
+        //GET:     api/commands/customId
+        [HttpGet("{Id}")]
+        public ActionResult<Command> GetCommandItem(int id)
+        {
+            var CommandItem = _context.CommandItems.Find(id);
+
+            if(CommandItem==null)
+            {
+                return NotFound();
+            }
+
+            return CommandItem;
+        }
+
+        //POST:  /api/commands
+
+        [HttpPost]
+        public ActionResult<Command> PostCommandItem(Command command)
+        {
+            _context.CommandItems.Add(command);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetCommandItem", new Command{Id=command.Id}, command);
+        }
+
+        //PUT:   /api/commands/customId
+
+        [HttpPut("{Id}")]
+        public ActionResult PutCommandItem(int id, Command command)
+        {
+            if(id != command.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(command).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+        //DELETE:   api/commands/customId
+
+        [HttpDelete("{Id}")]
+        public ActionResult<Command> DeleteCommandItem(int id)
+        {
+            var CommandItem = _context.CommandItems.Find(id);
+
+            if(CommandItem==null)
+            {
+                return NotFound();
+            }
+
+            _context.CommandItems.Remove(CommandItem);
+            _context.SaveChanges();
+
+            return CommandItem;
+        }
+
+        
+    }
+}
